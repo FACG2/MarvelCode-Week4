86687e8f7c0f5151ab3295e8b2adf5c3c528f4f5
EngAyoosh <aaa_2008azhar@hotmail.com>
2017-08-01 16:41:00 +0300

    build handler.js

---
+++ src/handler.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require ('path');
+
+ function serveHome(req, res){
+  readFile(res, '/index.html');
+};
+
+function readFile(res, endpoint) {
+  fs.readFile(path.join(__dirname, '..', 'public', endpoint), function(err, file) {
+    if (err) {
+      console.log(err);
+      notFound(null, res);
+    } else {
+      var extension = endpoint.split('.')[1];
+      var extensionType = {
+        "html": "text/html",
+        "css": "text/css",
+        "js": "application/javascript",
+      }
+      res.writeHead(200, { "Content-Type": extensionType[extension]});
+      res.end(file);
+    }
+  })
+}
+
+ function servePublic(req, res) {
+  var endpoint = req.url;
+  readFile(res, endpoint)
+};
+
+ function notFound(req, res){
+  res.writeHead(404,{'Content-Type': 'text/html'});
+  res.write('<h1>404 Page Requested Cannot Be Found<h1>');
+  res.end();
+};
+
+module.exports = {
+serveHome : serveHome,
+readFile : readFile,
+servePublic : servePublic,
+notFound : notFound,
+};
