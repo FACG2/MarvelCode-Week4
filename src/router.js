0d02f7c507a41f8721ffa0019a63555bd3166ab8
EngAyoosh <aaa_2008azhar@hotmail.com>
2017-08-01 16:42:43 +0300

    build server.js and router.js

---
+++ src/router.js
@@ -0,0 +1,15 @@
+var handler = require('./handler.js');
+var algorithm = require('./functions.js');
+
+module.exports = function(req, res) {
+  var endpoint = req.url;
+  if (endpoint === '/') {
+    handler.serveHome(req, res);
+  }
+  else if (endpoint.indexOf('search') !== -1) {
+    algorithm.serveHints(req, res);
+  }
+  else {
+    handler.servePublic(req,res);
+  }
+}
