8dc509dedaf4c64bde29d2ea0f89218a9997fb41
mrm7moud <mrm7moud@hotmail.com>
2017-08-02 09:19:48 +0300

    add css file and html file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 var http = require('http');
 var router = require('./router.js');
+const fs = require('fs');
+const path = require('path');
+
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 4000;
+
+
 var app = function(req, res){
   router(req,res);
 };
-
-var port = process.env.PORT || 4000;
 var server = http.createServer(app).listen(port);
-console.log('Server is listening on port: '+port);
+
+console.log('server running on: http://' + host + ':' + port);
