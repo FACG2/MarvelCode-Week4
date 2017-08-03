1090878dc73d7eaa9738e3c95121faf69bdccb02
Abdallah <you@example.com>
2017-08-03 11:38:27 +0300

    remove unused libs relates #31

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 var http = require('http');
 var router = require('./router.js');
-const fs = require('fs');
-const path = require('path');
+
 
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 4000;
 
 
-var app = function(req, res){
-  router.router(req,res);
-};
-var server = http.createServer(app).listen(port);
-
-console.log('server running on: http://' + host + ':' + port);
+var server = http.createServer(router.router)
+server.listen(port, () => {
+    console.log('server running on: http://' + host + ':' + port);
+});
